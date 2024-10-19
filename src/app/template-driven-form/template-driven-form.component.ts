fd90ead4a3b618b6e97b81ca84f0cd6bf2183eba
Emircan <emircandv787@gmail.com>
2024-10-19 20:48:49 +0300

    The template driven fom has been completed.

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AfterViewInit, Component, OnInit, ViewChild, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -10,6 +10,38 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
 })
-export class TemplateDrivenFormComponent {
+export class TemplateDrivenFormComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+
+  @ViewChild("frm", { static: true }) frm!: NgForm;
+
+
+  onSubmit(data: any): void {
+    console.log(data);
+    console.log(this.frm.value);
+    console.log(this.frm.submitted);
+    console.log(this.frm.touched);
+    console.log(this.frm.valid);
+
+
+
+  }
+
+  formReset(): void {
+    this.frm.reset();
+  }
+
+  formSetValue(): void {
+    // this.frm.form.patchValue({
+    //   Email: "setvaluemail"
+    // });
+
+    this.frm.control.patchValue({
+      Email: "setvaluemail"
+    });
+  }
 
 }
